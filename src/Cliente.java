3d954eae9f7f14bdd30f27161f02168757de5a2b
KingKing2019 <mf3115493@gmail.com>
2022-10-20 20:28:33 -0300

    adding cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,37 @@
+import java.util.Date;
 public class Cliente {
-    
+	private String nome;
+	private String cpf;
+	private Date dataNasc;
+	private String endereco;
+	
+	
+	public Cliente (String nome,Date dataNasc,String cpf,String endereco){
+        this.nome = nome;
+        this.dataNasc = dataNasc;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
 }
